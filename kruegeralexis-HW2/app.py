96fa1e44fe24cfb7f9b61e29d70c66b07e0e9f6f
alexis <alexismk0327@gmail.com>
2025-04-20 17:20:28 -0700

    Fixed links

--- kruegeralexis-HW2/app.py
+++ kruegeralexis-HW2/app.py
@@ -3,16 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def default():
-   return render_template('default.html', about="http://localhost:5000/about", 
-   form="http://localhost:5000/form")
+   return render_template('default.html')
 
 @app.route('/about')
 def about():
-   return render_template('about.html', default="http://localhost:5000/")
+   return render_template('about.html')
 
 @app.route('/form')
 def form():
-   return render_template('form.html', default="http://localhost:5000/")
+   return render_template('form.html')
 
 @app.route('/form/result', methods =["GET", "POST"])
 def gfg():
@@ -21,7 +20,7 @@ def gfg():
        first_name = request.form.get("fname")
        # getting input with name = lname in HTML form 
        return render_template("result.html", first_name = request.form.get("fname"), 
-       last_name = request.form.get("lname"), default="http://localhost:5000/")
+       last_name = request.form.get("lname"))
 
 if __name__ == '__main__':
    app.run()
