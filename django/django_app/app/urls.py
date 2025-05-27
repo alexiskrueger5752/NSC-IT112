4adec1c25f3a36974d152048edd5bbd019b7b038
alexis <alexismk0327@gmail.com>
2025-05-26 18:42:42 -0700

    templates

--- django/django_app/app/urls.py
+++ django/django_app/app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import app_list
+from .views import home_page
 
 urlpatterns = [
-    path("", app_list)
+    path("app_list/", app_list),
+    path('', home_page, name='home')
+
 ]
--- django/django_app/app/views.py
+++ django/django_app/app/views.py
@@ -6,4 +6,8 @@ def app_list(request):
     context = {
         'app_list' : post
     }
-    return render(request, "app/app_list.html", context)
+    return render(request, "app_list.html", context)
+
+def home_page(request):
+    user_name = request.GET.get('user_name')
+    return render(request, 'base.html', {'user_name': user_name})
