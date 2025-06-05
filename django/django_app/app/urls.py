609984060b6286689ce0e1cc4048ca435ee79c57
alexis <alexismk0327@gmail.com>
2025-06-05 00:45:13 -0700

    added django api

--- django/django_app/app/urls.py
+++ django/django_app/app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home_page, movie_list, movie_details
+from .views import home_page, movie_list, movie_details, MovieListCreate, MovieDetail, MovieByQuery
 
 urlpatterns = [
     path('', home_page, name='home'),
-    path("movies/", movie_list, name='movie_list'),
-    path("movies/movie_details/<int:movie_id>", movie_details, name='movie_details'),
+    path("movies_page/", movie_list, name='movie_table'),
+    path("movies/movie_details/<int:movie_id>", movie_details, name='movie_table_details'),
+    path('movies/',  MovieListCreate.as_view(), name='movie_list'),
+    path('movies/<int:pk>/', MovieDetail.as_view(), name='movie_details'),
+    path('movies/query/', MovieByQuery.as_view(), name='movie_by_query'),
 ]
--- django/django_app/app/views.py
+++ django/django_app/app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from .models import Movie
+from rest_framework import generics
+from .serializers import MovieSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 
 # Create your views here.
 def movie_list(request):
@@ -18,3 +24,23 @@ def movie_details(request, movie_id):
 def home_page(request):
     user_name = request.GET.get('user_name')
     return render(request, 'home.html', {'user_name': user_name})
+
+class MovieListCreate(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MovieByQuery(APIView):
+    def get(self, request):
+        movie_id = request.query_params.get('id')
+        if not movie_id:
+            return Response({"error": "Missing id query parameter"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            movie = Movie.objects.get(id=movie_id)
+            serializer = MovieSerializer(movie)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Movie.DoesNotExist:
+            return Response({"error": "Movie not found"}, status=status.HTTP_404_NOT_FOUND)
